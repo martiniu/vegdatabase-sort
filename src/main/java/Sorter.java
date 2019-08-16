184d3026877be059cd0e71f4a83f20c0d8c847ca
martiniu <martine@uriansrud.net>
2019-08-16 13:07:32 +0200

    print data

--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -1,18 +1,32 @@
 import no.vegvesen.nvdbapi.client.clients.ClientFactory;
 import no.vegvesen.nvdbapi.client.clients.RoadObjectClient;
+import no.vegvesen.nvdbapi.client.model.datakatalog.DataType;
 import no.vegvesen.nvdbapi.client.model.roadobjects.RoadObject;
 
 public class Sorter {
-    public void openDatabase() throws Exception {
+    public static void main(String[] args) {
+        Sorter.openDatabase();
+    }
+
+    public static void openDatabase() {
         // First, create factory
         ClientFactory factory = new ClientFactory("https://www.vegvesen.no/nvdb/api/v2", "nvdbapi-client", "ACME");
-    // Then, create your client. Typically, there's one per root endpoint
+        // Then, create your client. Typically, there's one per root endpoint
         RoadObjectClient client = factory.createRoadObjectClient();
 
-    // Example single object download
-        RoadObject ro = client.getRoadObject(534, 1);
+        for (DataType dt : client.getDatakatalog().getDataTypes()) {
+            System.out.println("[" +dt.getId() + "]: " + dt.getName() + " \nDesc: " + dt.getDescription());
+        }
+        // Example single object download
+        // RoadObject ro = client.getRoadObject(534, 1);
 
-    // Remember to close your factory when you're done using it
-        factory.close();
+        // Remember to close your factory when you're done using it
+
+        try {
+            factory.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
