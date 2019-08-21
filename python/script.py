523df61e28cdc4b55f4f3506f85945d4d23ebb04
Bartek <bartosz.tracz@knowit.no>
2019-08-21 16:31:37 +0200

    added GUI

--- python/script.py
+++ python/script.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import sys
 from pprint import pprint
 import webbrowser
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QMainWindow, QMessageBox, QLabel
+from PyQt5.QtCore import pyqtSlot
 
 # Drammen  -> Oslo
 # 86512932 -> 86513964
@@ -18,11 +21,80 @@ import webbrowser
 # 1_0 Blå 
 # 0_1 Blå?
 
+class App(QMainWindow):
 
-dekkebredde = input("Dekkebredde større enn: ")
+  def __init__(self):
+    super().__init__()
+    self.title = 'PyQt5 textbox - pythonspot.com'
+    self.layout = QVBoxLayout()
+    self.window = QWidget()
+    self.initUI()
+    
+  def initUI(self):
+    self.setWindowTitle(self.title)
+    #self.setGeometry(self.left, self.top, self.width, self.height)
+  
+    # Vegbredde
+    self.vegbredde_label = QLabel('Vegbredde, totalt')
+    self.vegbredde = QLineEdit(self)
+    self.layout.addWidget(self.vegbredde_label)
+    self.layout.addWidget(self.vegbredde)
 
-base_url = "https://www.vegvesen.no/vegkart/vegkart/#kartlag:geodata"
-edit_url = "/hva:(~(farge:'2_2,filter:(~(operator:'*3d,type_id:4566,verdi:(~5492)),(operator:'*3d,type_id:4568,verdi:(~18))),id:532),(farge:'0_1,filter:(~(operator:'*3e*3d,type_id:5555,verdi:(~"+str(dekkebredde)+"))),id:583))"
-constant_url = "/hvor:(kommune:(~301,220,219,602,626))/@250164,6638305,9/vegobjekt:83641744:40a744:583"
+    # Dekkebredde
+    self.dekkebredde_label = QLabel('Dekkebredde')
+    self.dekkebredde = QLineEdit(self)
+    self.layout.addWidget(self.dekkebredde_label)
+    self.layout.addWidget(self.dekkebredde)
 
-webbrowser.open(base_url+edit_url+constant_url)
+    # Kjørebanebredde
+    self.kjorebanebredde_label = QLabel('Kjørebanebredde')
+    self.kjorebanebredde = QLineEdit(self)
+    self.layout.addWidget(self.kjorebanebredde_label)
+    self.layout.addWidget(self.kjorebanebredde)
+      
+    # Create a button in the window
+    self.button = QPushButton('Søk', self)
+    self.layout.addWidget(self.button)
+    
+    # Update Window
+    self.window.setLayout(self.layout)
+
+    # connect button to function on_click
+    self.button.clicked.connect(self.on_click)
+    self.window.show()
+    
+  @pyqtSlot()
+  def on_click(self):
+    vegbredde_value = self.vegbredde.text()
+    #QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
+    #self.textbox.setText("")
+    base_url = "https://www.vegvesen.no/vegkart/vegkart/#kartlag:geodata"
+    edit_url = "/hva:(~(farge:'2_2,filter:(~(operator:'*3d,type_id:4566,verdi:(~5492)),(operator:'*3d,type_id:4568,verdi:(~18))),id:532),(farge:'0_1,filter:(~(operator:'*3e*3d,type_id:5555,verdi:(~"+str(vegbredde_value)+"))),id:583))"
+    constant_url = "/hvor:(kommune:(~301,220,219,602,626))/@250164,6638305,9/vegobjekt:83641744:40a744:583"
+
+    webbrowser.open(base_url+edit_url+constant_url)
+
+
+if __name__ == '__main__':
+  app = QApplication(sys.argv)
+  ex = App()
+  sys.exit(app.exec_())
+
+# app = QApplication([])
+# window = QWidget()
+
+# asd = QLineEdit()
+# layout.addWidget(asd)
+# layout.addWidget(QPushButton('Top'))
+# layout.addWidget(QPushButton('Bottom'))
+# window.setLayout(layout)
+# window.show()
+
+# print(asd.text())
+
+
+
+#dekkebredde = input("Dekkebredde større enn: ")
+
+
+#app.exec_()
