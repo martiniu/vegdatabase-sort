bf030e332ed89a42ebe9d467ac9645b80941d92f
martiniu <martine@uriansrud.net>
2019-08-22 08:47:33 +0200

    added todo and bugissue to pythonscript

--- python/script.py
+++ python/script.py
@@ -18,7 +18,7 @@ from PyQt5.QtCore import pyqtSlot
 
 # 0_0 Grønn
 # 2_2 Rødt
-# 1_0 Blå 
+# 1_0 Blå
 # 0_1 Blå?@
 
 class App(QMainWindow):
@@ -29,11 +29,11 @@ class App(QMainWindow):
     self.layout = QVBoxLayout()
     self.window = QWidget()
     self.initUI()
-    
+
   def initUI(self):
     self.setWindowTitle(self.title)
     #self.setGeometry(self.left, self.top, self.width, self.height)
-  
+
     # Vegbredde
     self.vegbredde_label = QLabel('Vegbredde, totalt')
     self.vegbredde = QLineEdit(self)
@@ -51,24 +51,28 @@ class App(QMainWindow):
     self.kjorebanebredde = QLineEdit(self)
     self.layout.addWidget(self.kjorebanebredde_label)
     self.layout.addWidget(self.kjorebanebredde)
-      
+
     # Create a button in the window
     self.button = QPushButton('Søk', self)
     self.layout.addWidget(self.button)
-    
+
     # Update Window
     self.window.setLayout(self.layout)
 
     # connect button to function on_click
     self.button.clicked.connect(self.on_click)
     self.window.show()
-    
+
   @pyqtSlot()
   def on_click(self):
     vegbredde_value = self.vegbredde.text()
     #QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
     #self.textbox.setText("")
     base_url = "https://www.vegvesen.no/vegkart/vegkart/#kartlag:geodata"
+
+    #todo:  add different ID for vegbredde totalt(5264), dekkebredde(5555), kjørebanebredde(5556)
+    #bug:   when adding a value to the input box, it will search correctly
+    #       the first time, but not the second, the value will be blank in the browser filter bar
     edit_url = "/hva:(~(farge:'2_2,filter:(~(operator:'*3d,type_id:4566,verdi:(~5492)),(operator:'*3d,type_id:4568,verdi:(~18))),id:532),(farge:'0_1,filter:(~(operator:'*3e*3d,type_id:5555,verdi:(~"+str(vegbredde_value)+"))),id:583))"
     constant_url = "/hvor:(kommune:(~301,220,219,602,626))/@250164,6638305,9/vegobjekt:83641744:40a744:583"
 
