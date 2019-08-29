3103a79379c4af4f80d5eedc33d9c9b23e84d4ce
martiniu <martine@uriansrud.net>
2019-08-29 12:52:55 +0200

    Changed file structure and edited readme.md

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # vegdatabase-sort
 Sorts and analyzes Nasjonal vegdatabank with the vegdatabase api (https://api.vegdata.no/).
 
+To use this program, you will firstly need to run ```file_sorter.py``` in order to update data to be as new as possible. After that you can run ```script.py``` and input your desired value.
+
+# How to use
+**Install python3**
+- If you have brew, run ```brew install python3``` followed by ```python3 --version```. The version should be 3.7.4 or newer. If you don't have brew, search Google for ```install brew <operating system>``` and follow the first guide. When writing ```brew doctor```, the command line should say ```Your system is ready to brew```
+
 # objectwiki.txt
 Contains a list of all RoadObjects as well as each RoadObjects' attributeType and their id. This is the easiest way to understand what id belongs to what name. It makes it easier to create queries, simply search for the id or the name of the object or feature you are looking for.
 
@@ -8,16 +14,16 @@ Contains a list of all RoadObjects as well as each RoadObjects' attributeType an
 Contains a list of query examples, with short explanations of each id and value used. The NVDB documentation site gives a general understanding of how to write queries: https://api.vegdata.no/.
 
 # python
-- script.py
+- **script.py**
   - Contains a script which will allow the user to easily enter a width and then be redirected to https://www.vegvesen.no/nvdb/vegkart/v2 to look at a map with details of the road with the given width filter. The second tab of this program also allows the user to view statistics about their search. Currently the most important file used by this script is ```files/query_vegref_med_felt.json```
 
-- file_sorter.py
+- **file_sorter.py**
   - Contains a script that sorts through information and saves it for easy use. This information is used in script.py to display statistics about the given input.
 
-- other
+- **other**
   - files: contains .json files for script.py and file_sorter.py to use.
-  - old_files: contains old .json files that are not used by script.py or file_sorter.py, but that have been used at one point.
+  - unused: contains old .json files and old methods previously used in file_sorter.py
 
 # java
-- InfoCollectorNVDB.java
+- **InfoCollectorNVDB.java** and **Main.java**
   - Contains a couple methods to collect important information about RoadObject types and  their Attribute types. This method prints the information to objectwiki.txt.
--- python/file_sorter.py
+++ python/file_sorter.py
@@ -19,14 +19,14 @@ def create_object_from_href(href):
                 egenskaper =    {
                     "ant_felt": egenskap['verdi'],
                 }
-            if egenskap['navn'] == 'Vegkategori':
-                egenskaper = {
-                    "vegkategori": egenskap['verdi'],
-                }
-            if egenskap['navn'] == 'Vegstatus':
-                egenskaper['vegstatus'] = egenskap['verdi']
-            if egenskap['navn'] == 'Vegnummer':
-                egenskaper['vegnummer'] = egenskap['verdi']
+            # if egenskap['navn'] == 'Vegkategori':
+            #     egenskaper = {
+            #         "vegkategori": egenskap['verdi'],
+            #     }
+            # if egenskap['navn'] == 'Vegstatus':
+            #     egenskaper['vegstatus'] = egenskap['verdi']
+            # if egenskap['navn'] == 'Vegnummer':
+            #     egenskaper['vegnummer'] = egenskap['verdi']
 
         egenskaper['veglenkeid'] = response_objekt['lokasjon']['stedfestinger'][0]['veglenkeid']
         vegobjekt[o['id']] = egenskaper
@@ -34,7 +34,6 @@ def create_object_from_href(href):
 
     # if 1000 objects were returned, get the link to the next batch of objects and loop through it
     if response['metadata']['returnert'] == 1000:
-        #response = requests.get(response['metadata']['neste']['href']).json()
         create_object_from_href(response['metadata']['neste']['href'])
 
 def write_href_to_file(link, filename):
@@ -52,21 +51,6 @@ def write_dict_to_file(dict, filename):
     with open(filename, 'w') as writeobject:
         json.dump(dict, writeobject)
 
-def create_all_dekkebredder():
-    oslo = get_file_as_json('files/dekkebredde_oslo.json')
-    asker = get_file_as_json('files/dekkebredde_asker.json')
-    lier = get_file_as_json('files/dekkebredde_lier.json')
-    drammen = get_file_as_json('files/dekkebredde_drammen.json')
-    bærum = get_file_as_json('files/dekkebredde_bærum.json')
-
-    oslo.update(asker)
-    oslo.update(lier)
-    oslo.update(drammen)
-    oslo.update(bærum)
-
-    dekkebredder = copy.deepcopy(oslo)
-    return dekkebredder
-
 def create_all_felt():
     oslo = get_file_as_json('files/felt_oslo.json')
     asker = get_file_as_json('files/felt_asker.json')
@@ -81,22 +65,6 @@ def create_all_felt():
 
     felt = copy.deepcopy(oslo)
     return felt
-
-def write_all_dekkebredder():
-    dekkebredde_oslo = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/583?fylke=3'
-    dekkebredde_asker = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/583?kommune=220'
-    dekkebredde_lier = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/583?kommune=626'
-    dekkebredde_drammen = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/583?kommune=602'
-    dekkebredde_bærum = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/583?kommune=219'
-
-    write_href_to_file(dekkebredde_oslo, 'files/dekkebredde_oslo.json')
-    write_href_to_file(dekkebredde_asker, 'files/dekkebredde_asker.json')
-    write_href_to_file(dekkebredde_lier, 'files/dekkebredde_lier.json')
-    write_href_to_file(dekkebredde_drammen, 'files/dekkebredde_drammen.json')
-    write_href_to_file(dekkebredde_bærum, 'files/dekkebredde_bærum.json')
-
-    dekkebredder = create_all_dekkebredder()
-    write_dict_to_file(dekkebredder, 'files/dekkebredder_alle.json')
 
 def write_all_felt():
     felt_oslo = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/482?fylke=3'
@@ -113,61 +81,6 @@ def write_all_felt():
 
     felt = create_all_felt()
     write_dict_to_file(felt, 'files/felt_alle.json')
-
-def merge_dekkebredde_felt():
-    dekkebredder = get_file_as_json('files/dekkebredder_alle.json')
-    felt = get_file_as_json('files/felt_alle.json')
-
-    dekkebredder_med_felt = copy.deepcopy(dekkebredder)
-
-    for d in dekkebredder:
-        felt_liste = []
-        for f in felt:
-            if dekkebredder_med_felt[d]['veglenkeid'] == felt[f]['veglenkeid']:
-                # legg til alle antall felt i en midlertidlig liste per dekkebredde objekt
-                felt_liste.append(felt[f]['ant_felt'])
-
-        felt_liste = list(dict.fromkeys(felt_liste))
-        dekkebredder_med_felt[d]['ant_felt'] = felt_liste
-
-    test_merge = copy.deepcopy(dekkebredder_med_felt)
-    write_dict_to_file(test_merge, 'files/dekkebredde_felt.json')
-
-def write_all_E18():
-    vegref = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/532?egenskap=(4566=5492 AND 4568=18 AND 4570=5506)'
-
-    write_href_to_file(vegref, 'files/vegref.json')
-
-def merge_dekkebredde_felt_vegref():
-    merge = get_file_as_json('files/dekkebredde_felt.json')
-    vegref = get_file_as_json('files/vegref_e18.json')
-
-    merge_final = copy.deepcopy(merge)
-
-    for m in merge:
-        for v in vegref:
-            if merge_final[m]['veglenkeid'] == vegref[v]['veglenkeid']:
-                merge_final[m]['vegkategori'] = vegref[v]['vegkategori']
-                merge_final[m]['vegnummer'] = vegref[v]['vegnummer']
-                merge_final[m]['vegstatus'] = vegref[v]['vegstatus']
-
-
-    final_merge = copy.deepcopy(merge_final)
-    write_dict_to_file(final_merge, 'files/dekkebredder_felt_og_e18.json')
-
-def filter_out_nonvegref():
-    final = get_file_as_json('files/dekkebredder_felt_og_e18.json')
-
-    final_copy = copy.deepcopy(final)
-
-    for f in final:
-        #pprint(final)
-        if 'vegkategori' in final[f]:
-            pass
-        else:
-            del final_copy[f]
-
-    write_dict_to_file(final_copy, 'files/dekkebredder_felt_kun_e18.json')
 
 def write_query_vegref():
     query_vegref = 'https://www.vegvesen.no/nvdb/api/v2/vegobjekter/583?egenskap="5555>=0"&vegreferanse=EV18&kommune=301&kommune=220&kommune=219&kommune=602&kommune=626'
@@ -198,11 +111,6 @@ if __name__ == '__main__':
 
     vegobjekt = {}
 
-    #write_all_dekkebredder()
-    #write_all_felt()
-    #merge_dekkebredde_felt()
-    #write_all_E18()
-    #merge_dekkebredde_felt_vegref()
-    #filter_out_nonvegref()
+    write_all_felt()
     write_query_vegref()
     merge_query_vegref_felt()
